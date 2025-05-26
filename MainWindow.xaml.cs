6f3b5cd0d15969bed3b670404bc4baea4fa18759
Nikita <nikitagol19@gmail.com>
2025-05-26 10:47:20 +0300

    Added Views

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LuxuryCarRental.Views;
 
 namespace LuxuryCarRental;
 
@@ -19,5 +20,24 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        NavTabs.SelectionChanged += (s, e) =>
+        {
+            switch (NavTabs.SelectedIndex)
+            {
+                case 0: ContentArea.Content = new CatalogView(); break;
+                case 1: ContentArea.Content = new CategoryView(); break;
+                case 2: ContentArea.Content = new CartView(); break;
+                case 3: ContentArea.Content = new CheckoutView(); break;
+                case 4: ContentArea.Content = new ConfirmationView(); break;
+                case 5: ContentArea.Content = new DealsView(); break;
+                default:
+                    ContentArea.Content = null;
+                    break;
+            }
+        };
+
+        // show first tab by default
+        NavTabs.SelectedIndex = 0;
     }
 }
